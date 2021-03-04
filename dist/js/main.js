0d20788b7627b60247fb0f36ed9ea7dc4ddaf87b
wolfie Dedev <wolfiededev@kodeds-iMac.localdomain>
2021-03-04 17:31:23 +0000

    modified javascript to be in strict mode

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,16 +1,17 @@
-const btnCheckbox = document.querySelector('#btn-checkbox');
-const monthly = document.querySelectorAll('.monthly');
-const annually = document.querySelectorAll('.annually');
+"use strict";
+const btnCheckbox = document.querySelector("#btn-checkbox");
+const monthly = document.querySelectorAll(".monthly");
+const annually = document.querySelectorAll(".annually");
 
 const switcher = () => {
   if (btnCheckbox.checked === true) {
-    monthly.forEach(month => (month.style.display = 'none'));
-    annually.forEach(annual => (annual.style.display = 'block'));
+    monthly.forEach((month) => (month.style.display = "none"));
+    annually.forEach((annual) => (annual.style.display = "block"));
   }
   if (btnCheckbox.checked === false) {
-    monthly.forEach(month => (month.style.display = 'block'));
-    annually.forEach(annual => (annual.style.display = 'none'));
+    monthly.forEach((month) => (month.style.display = "block"));
+    annually.forEach((annual) => (annual.style.display = "none"));
   }
 };
 
-btnCheckbox.addEventListener('click', switcher);
+btnCheckbox.addEventListener("click", switcher);
