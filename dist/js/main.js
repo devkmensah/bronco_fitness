beb48caa53a7ab2cd669d999e16472b817a9994e
wolfie Dedev <wolfiededev@kodeds-iMac.localdomain>
2021-03-04 15:43:22 +0000

    javascript code properly recovered

--- dist/js/main.js
+++ dist/js/main.js
@@ -2,20 +2,15 @@ const btnCheckbox = document.querySelector('#btn-checkbox');
 const monthly = document.querySelectorAll('.monthly');
 const annually = document.querySelectorAll('.annually');
 
+const switcher = () => {
+  if (btnCheckbox.checked === true) {
+    monthly.forEach(month => (month.style.display = 'none'));
+    annually.forEach(annual => (annual.style.display = 'block'));
+  }
+  if (btnCheckbox.checked === false) {
+    monthly.forEach(month => (month.style.display = 'block'));
+    annually.forEach(annual => (annual.style.display = 'none'));
+  }
+};
 
-const switcher = function(){
-
-   for (let i = 0; i < monthly.length; i++){
-    if(btnCheckbox.checked === true) {
-        annually[i].style.display = 'block';
-        monthly[i].style.display = 'none';
-    } else if(btnCheckbox.checked === false){
-        annually[i].style.display = 'none';
-        monthly[i].style.display = 'block';
-    }
-   }
-
-}
-
-switcher()
-
+btnCheckbox.addEventListener('click', switcher);
