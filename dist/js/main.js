4026efcee960e839b3c234649039ace2d6cb6b15
devkmensah <kmensah.dev@gmail.com>
2021-03-16 17:10:36 +0000

    deleted all the arrow-up icon functionality including its javascript file

--- dist/js/main.js
+++ dist/js/main.js
@@ -17,20 +17,3 @@ const switcher = () => {
   }
 };
 btnCheckbox.addEventListener('click', switcher);
-
-// 2. Smooth scroll effects for about us nav & footer arrow btn
-const aboutNav = document.querySelector('.about-nav');
-const arrow = document.querySelector('.arrow');
-const footer = document.querySelector('#footer');
-const header = document.querySelector('#header');
-console.log(aboutNav, arrow, footer, header);
-
-aboutNav.addEventListener('click', function (e) {
-  e.preventDefault();
-  footer.scrollIntoView({ behavior: 'smooth' });
-});
-
-arrow.addEventListener('click', function (e) {
-  e.preventDefault();
-  header.scrollIntoView({ behavior: 'smooth' });
-});
